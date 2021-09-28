458d3a8c7aa1c8f6a4b83be826dd2c7a5061c0f6
Rising-Phoenix-Ltd <risingphoenix.web.in@gmail.com>
2021-09-28 14:21:36 +0530

    Final Commit

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from flask import Flask, render_template, redirect, url_for, flash, abort, request
 from flask_bootstrap import Bootstrap
-from flask_ckeditor import CKEditor
-from datetime import date
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
-from flask_gravatar import Gravatar
 import os
 
 # --- Constants ---
